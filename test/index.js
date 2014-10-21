5143141332d64c9ebcc60c7c7eab651ad8dcd84c
Ben Drucker <bvdrucker@gmail.com>
2014-10-21 08:08:39 -0400

    Rewrite tests to allow for other scenarios

--- test/index.js
+++ test/index.js
@@ -1,26 +1,25 @@
 'use strict';
 
-var browserify = require('browserify');
-var expect     = require('chai').expect;
+var browserify      = require('browserify');
+var streamToPromise = require('stream-to-promise');
+var expect          = require('chai').expect;
 
 describe('proxyquire-universal', function () {
 
-  var bundle;
-  beforeEach(function (done) {
-    browserify()
+  function bundle (scenario) {
+    return streamToPromise(browserify()
       .plugin(require('../'))
-      .add(__dirname + '/fixtures/test.js')
-      .bundle(function (err, _bundle_) {
-        if (err) return done(err);
-        bundle = _bundle_.toString();
-        done();
-      });
-  });
+      .add(__dirname + '/fixtures/' + scenario + '.js')
+      .bundle())
+      .call('toString');
+  };
 
   it('rewrites proxyquire calls', function () {
-    expect(bundle)
-      .to.contain('require(\'proxyquireify\')(require)')
-      .and.to.not.contain('require(\'proxyquire\')');
+    return bundle('default').then(function (bundle) {
+      expect(bundle)
+        .to.contain('require(\'proxyquireify\')(require)')
+        .and.to.not.contain('require(\'proxyquire\')');
+    });
   });
 
 });
