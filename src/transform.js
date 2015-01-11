e8079916db7e5c8b7d9a4011286b8b88c7a52676
Ben Drucker <bvdrucker@gmail.com>
2015-01-11 12:08:20 -0500

    Use transformify to handle buffering and sync jstransform call

--- src/transform.js
+++ src/transform.js
@@ -1,20 +1,15 @@
 'use strict';
 
-var through     = require('through2');
-var jstransform = require('jstransform');
-var visitor     = require('./visitor');
+var through      = require('through2');
+var transformify = require('transformify');
+var jstransform  = require('jstransform');
+var visitor      = require('./visitor');
 
 module.exports = function (file, options) {
   if (/\.json$/.test(file)) return through();
-
-  var code = '';
-  function read (chunk, enc, next) {
-    code += chunk;
-    next();
-  }
-  function flush (next) {
-    this.push(jstransform.transform([visitor], code).code);
-    next();
-  }
-  return through(read, flush);
+  return transformify(replaceProxyquire)();
 }
+
+function replaceProxyquire (code) {
+  return jstransform.transform([visitor], code).code
+} 
